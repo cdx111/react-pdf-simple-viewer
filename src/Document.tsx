8265390cb5dcd370156f3e40c4752acc2f00da01
h <201910005@qq.com>
2021-11-22 22:43:10 +0800

    first commit

---
+++ src/Document.tsx
@@ -0,0 +1,5 @@
+import * as pdfjs from 'pdfjs-dist';
+export const Document: React.FC = () => {
+  console.log(pdfjs);
+  return null;
+};
---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import * as pdfjs from 'pdfjs-dist';
+// // Delete me
+export const Thing = () => {
+  console.log(pdfjs);
+  const a = { a: 1 };
+  console.log(a?.a);
+  return <div>the snozzberries taste like snozzberries</div>;
+};
