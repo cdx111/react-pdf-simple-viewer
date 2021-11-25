63934eb7632fb5010a375186f59426b9c52d5164
caidunxin <caidunxin@shushangai.com>
2021-11-25 10:37:11 +0800

    page

--- src/documentContext.ts
+++
@@ -1,6 +0,0 @@
-import { PDFDocumentProxy } from 'pdfjs-dist/types/src/display/api';
-import React from 'react';
-
-export const DocumentContext = React.createContext<PDFDocumentProxy | null>(
-  null
-);
--- src/index.tsx
+++
@@ -1,6 +0,0 @@
-import { Document } from './Document';
-import { Page } from './Page';
-import * as pdfjs from 'pdfjs-dist';
-export { pdfjs, Document, Page };
-
-export default { pdfjs, Document, Page };
